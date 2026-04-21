459325fc88f39a797ba83c55826d1aa6cd3b85b4
Adham Behairy <84999874+adhambehairy@users.noreply.github.com>
2026-04-21 11:37:12 +0200

    Add files via upload

---
+++ Countdown_Timer.py
@@ -0,0 +1,19 @@
+import time
+from plyer import notification
+def countdown(t):
+    while t:
+        mins, secs= divmod(t,60)
+        timer= '{:02d}:{:02d}'.format(mins,secs)
+        print(timer,end='\r')
+        time.sleep(1)
+        t-=1
+        
+    print("Fire in the hole!")
+    
+    notification.notify(
+        title="Timer Finish!!",
+        message="Great job",
+        timeout=5
+    )
+t= int(input("Enter the time in seconds: "))    
+countdown(t)    
